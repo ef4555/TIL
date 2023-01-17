e6531f3371faf60165a49add9dfdbb896ef2c9f9
ef4555 <chldgns9806@gmail.com>
2023-01-17 15:17:32 +0900

    update

---
+++ test.py
@@ -0,0 +1,12 @@
+'''
+a = int(input("첫번째 숫자 입력해주세요 : "))
+b = int(input("두번째 숫자 입력해주세요 : "))6374
+
+print(a+b)
+'''
+'''
+lunch_dict = {'밥' : 100, '빵' : 33}
+b = list(lunch_dict.values())
+a = sum(lunch_dict.values())
+print(a/len(b))
+'''
