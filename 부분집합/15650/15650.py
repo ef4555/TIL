17babcfd47266951ab740618d58a53ee3fa9fa84
ef4555 <chldpgns9806@gmail.com>
2023-03-31 00:44:48 +0900

    0330

--- 부분집합/15650/15650.py
+++ 부분집합/15650/15650.py
@@ -1,5 +1,6 @@
-def c(x, arr):
-    if sum(arr) == M:
+def c(s, x, arr):
+    if x == M:
+        # print(arr)
         lst2 = []
         # print(arr)
         for i in range(N):
@@ -9,10 +10,10 @@ def c(x, arr):
                 
         return
     
-    for k in range(x, N):
+    for k in range(s, N):
         if arr[k] != 1:
             arr[k] = 1
-            c(k, arr)
+            c(k+1, x+1, arr)
             arr[k] = 0
     return
 
@@ -20,5 +21,5 @@ N, M = map(int, input().split())
 # N까지의 자연수에서 M개 뽑기
 lst = [i for i in range(1, N+1)]
 arr = [0]*N
-c(0, arr)
+c(0, 0, arr) # 스타트, 카운트, 배열
 
